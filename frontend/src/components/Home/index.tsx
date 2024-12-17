ad2a9a0908a76df99b1af43deb3eb8a69c2d06b8
gabriel <gabriel.lealsantos@hotmail.com>
2024-12-17 11:59:38 -0300

    Home page

---
+++ frontend/src/components/Home/index.tsx
@@ -0,0 +1,27 @@
+import main from '../../assets/main.svg';
+import './styles.css';
+
+export default function Home() {
+    return (
+        <div className='home-container'>
+            <div className='home-content'>
+                <div className='home-actions'>
+                    <h1 className='home-title'>
+                        Faça seu pedido <br /> que entregamos <br />  pra você!!!
+                    </h1>
+                    <h3 className='home-subtitle'>
+                        Escolha o seu pedido e em poucos minutos <br />
+                        levaremoss na sua porta
+                    </h3>
+
+                    <a href="order" className='home-btn-order'>
+                        FAZER PEDIDO
+                    </a>
+                </div>
+                <div className='home-img'>
+                    <img src={main} alt="main" />
+                </div>
+            </div>
+        </div>
+    );
+}
