84e4f91a90fbfca070d24207eaa5fcbe109546b6
gabriel <gabriel.lealsantos@hotmail.com>
2025-01-08 12:03:32 -0300

    Add page home page

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/Components/Header';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import Home from './src/Components/Home';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Home />
+      </GestureHandlerRootView>
     </View>
   );
 }
@@ -13,8 +27,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
